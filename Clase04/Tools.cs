6e566fe4622be890fce0b4d349dc31b4eac74d10
Luciano Gorosito <lucianogorosito@hotmail.com>
2017-11-28 20:23:53 -0300

    Update Tools.cs

--- Clase04/Tools.cs
+++ Clase04/Tools.cs
@@ -8,3 +8,17 @@ Document doc = uiDoc.Document;
 Reference r = uiDoc.Selection.PickObject(Autodesk.Revit.UI.Selection.ObjectType.Element, "Seleccionar un elemento");
 // Obtener el elemento elegido
 Element e = doc.GetElement(r.ElementId);
+
+// Obtener Parametros
+Parameter pComentario = _elem.get_Parameter(BuiltInParameter.ALL_MODEL_INSTANCE_COMMENTS);
+Parameter pMarca = _elem.get_Parameter(BuiltInParameter.DOOR_NUMBER);
+
+// Iniciar Nueva Transaccion
+Transaction t = new Transaction(_doc, "Cambiar Parametro");
+t.Start();
+
+// Modificar Parametros
+pComentario.Set(comentario);
+pMarca.Set(marca);
+
+t.Commit();
